2a5233cdc826d204f7d85ca18640c9568b3a5aae
unknown <developer@anb5.nl>
2025-02-18 13:07:02 +0100

    fix macos build

--- customization/generate_custom_text.py
+++ customization/generate_custom_text.py
@@ -91,7 +91,7 @@ pub fn is_incoming_only() -> bool {
         {
             'file': 'build.py',
             'from': "system2('cp -rf ../target/release/service ./build/macos/Build/Products/Release/RustDesk.app/Contents/MacOS/')",
-            'to':  f"system2('cp -rf ../target/release/service \"./build/macos/Build/Products/Release/{config.app_name}.app/Contents/MacOS/')\""
+            'to':  f"system2('cp -rf ../target/release/service \"./build/macos/Build/Products/Release/{config.app_name}.app/Contents/MacOS/\"')"
         },
 
         {
