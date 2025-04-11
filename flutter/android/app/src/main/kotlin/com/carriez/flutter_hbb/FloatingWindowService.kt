1589239fb5862edaaf1a1fd1033cbc9fc2c1cbd1
ANB5Dev <90337467+ANB5Dev@users.noreply.github.com>
2025-04-11 14:38:45 +0200

    Update FloatingWindowService.kt: undo change

--- flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/FloatingWindowService.kt
+++ flutter/android/app/src/main/kotlin/com/carriez/flutter_hbb/FloatingWindowService.kt
@@ -311,7 +311,7 @@ class FloatingWindowService : Service(), View.OnTouchListener {
              popupMenu.menu.add(0, idSyncClipboard, 0, translate("Update client clipboard"))
          }
          val idStopService = 2
-         // popupMenu.menu.add(0, idStopService, 0, translate("Stop service"))
+         popupMenu.menu.add(0, idStopService, 0, translate("Stop service"))
          popupMenu.setOnMenuItemClickListener { menuItem ->
              when (menuItem.itemId) {
                  idShowRustDesk -> {
